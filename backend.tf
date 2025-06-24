e8b3ad098ac756bdf56d577349317ec09b93bd71
rnjsdbwlsqwer <rnjsdbwls2006@naver.com>
2025-06-25 02:04:03 +0900

    Revert "Terraform backend setting for Organization"
    This reverts commit 06ca4d776049d1b98d4bbd2d8d003ebeed2fa1a9.

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudfence-tfstate-org"
-    key            = "organization/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "tfstate-lock-org"
-    encrypt        = true
-  }
-}
