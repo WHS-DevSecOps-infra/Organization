7d48ec869f1c5ff22893ce1de8b7bf209b445878
imyourhopeee <144752063+imyourhopeee@users.noreply.github.com>
2025-06-25 15:44:52 +0900

    test main.tf

--- identity-team-account/main.tf
+++ identity-team-account/main.tf
@@ -1,10 +1,10 @@
 # AWS 제공자 설정
 provider "aws" {
-    region = "ap-northeast-2"  # 리전 설정
+  region = "ap-northeast-2" # 리전 설정
 }
 
-# # S3 버킷 생성
-# resource "aws_s3_bucket" "my_bucket" {
+# S3 버킷 생성
+resource "aws_s3_bucket" "my_bucket" {
 
-#   bucket = "my-unique-bucket-name-terraform"  # 고유한 이름을 사용해야 합니다.
-# }
+  bucket = "my-unique-bucket-name-terraform" # 고유한 이름을 사용해야 합니다.
+}
