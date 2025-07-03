6ee751cf7341a9b27928b6a095c84125c62b0f4a
subin <su0201bin@naver.com>
2025-07-03 10:02:20 +0900

    S3 main.tf 수정

--- identity-team-account/iam/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudfence-identity-bucket"
-    key            = "iam/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "iam-identity-lock"
-  }
-}
