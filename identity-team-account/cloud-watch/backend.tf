9cf47caa1d4673f10c4a9e542333ca290f1f1222
subin <su0201bin@naver.com>
2025-07-04 13:58:21 +0900

    cicd 코드 추가

--- identity-team-account/cloud-watch/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudfence-identity-bucket"
-    key            = "cloudwatch/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "cloudwatch-identity-lock"
-  }
-}
