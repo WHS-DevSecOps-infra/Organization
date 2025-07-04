e7a9b7b23e1b02ed20dbf74babc29b7f6a290e12
subin <su0201bin@naver.com>
2025-07-04 20:38:20 +0900

    s3 & dynamodb

--- identity-team-account/ssoadmin/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudfence-identity-state"
-    key            = "ssoadmin/terraform.tfstate"
-    region         = "ap-northeast-2"
-    encrypt        = true
-    dynamodb_table = "ssoadmin-identity-lock"
-  }
-}
