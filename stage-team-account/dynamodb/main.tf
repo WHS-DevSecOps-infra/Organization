bb31a312cc3105c723e9202f35fe66b364eeaf30
subin <su0201bin@naver.com>
2025-07-06 17:08:35 +0900

    feature: 각 account의 s3 버킷과 dynamodb 생성

---
+++ stage-team-account/dynamodb/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+# 리소스 단위 Lock Table
+
+locals {
+  resources = [
+    "ecr",
+    "ec2",
+    "ecs",
+    "alb",
+    "cloudwatch",
+    "dynamodb",
+    "s3"
+  ]
+}
+
+resource "aws_dynamodb_table" "resource_locks" {
+  for_each     = toset(local.resources)
+  name         = "${each.key}-stage-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  # 서버 측 암호화 설정
+  server_side_encryption {
+    enabled = true # 서버 측 암호화 활성화
+  }
+
+  tags = {
+    Name        = "${each.key} Lock Table"
+    Environment = "stage"
+  }
+}
