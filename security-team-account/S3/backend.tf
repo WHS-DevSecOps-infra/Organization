d1deba51f32595a08d2599a40ccde257f234248a
subin <su0201bin@naver.com>
2025-07-06 18:13:02 +0900

    fix: terraform fmt issue in backend.tf

--- security-team-account/S3/backend.tf
+++ security-team-account/S3/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "cloudfence-security-state"      
-    key            = "s3/terraform.tfstate"            
+    bucket         = "cloudfence-security-state"
+    key            = "s3/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
-    dynamodb_table = "s3-security-lock"          
+    dynamodb_table = "s3-security-lock"
   }
 }
