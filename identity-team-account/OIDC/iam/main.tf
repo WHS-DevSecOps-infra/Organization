cd6a0b49ae885fe51df987b9ff564ae4f4b2135f
rnjsdbwlsqwer <rnjsdbwls2006@naver.com>
2025-07-08 23:19:29 +0900

    OIDC 코드화

---
+++ identity-team-account/OIDC/iam/main.tf
@@ -0,0 +1,74 @@
+# identity-team-account의 main.tf
+# modules/github_oidc를 불러와 해당account별 OIDC역할을 자동으로 생성하는 구조
+
+module "github_oidc" {
+  source = "../../../modules/github_oidc"
+
+  role_name = "Organization-role"
+
+  # GitHub Actions에서 이 role을 사용할 수 있도록 허용하는 sub조건
+  sub_condition = "repo:WHS-DevSecOps-infra/Organization:*"
+    
+
+  # 이 role에 연결할 정책들(IAM 정책 ARN)
+  policy_arns = [
+    "arn:aws:iam::aws:policy/AdministratorAccess"
+  ]
+}
+
+#tfsec:ignore:aws-iam-no-policy-wildcards
+resource "aws_iam_role_policy" "custom_inline_policy" {
+  name = "org-role"
+  role = module.github_oidc.oidc_role_name # 모듈에서 출력된 role이름 참조
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "sso:CreatePermissionSet",
+          "sso:DescribePermissionSet",
+          "sso:UpdatePermissionSet",
+          "sso:DeletePermissionSet",
+          "sso:AttachManagedPolicyToPermissionSet",
+          "sso:ListPermissionSets",
+          "sso:ListInstances",
+          "sso:ProvisionPermissionSet",
+          "sso:PutInlinePolicyToPermissionSet",
+          "sso:DeleteInlinePolicyFromPermissionSet",
+          "sso:ListPermissionSetProvisioningStatus",
+          "organizations:*",
+          "identitystore:*"
+        ],
+        Resource = "*"
+      },
+      {
+        "Sid" : "S3Access",
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:*"
+        ],
+        "Resource" : [
+          "*"
+        ]
+      },
+      {
+        "Sid" : "KMSAccess",
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:*"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "DynamoDBAccess",
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:*"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
---
+++ modules/github_oidc/main.tf
@@ -0,0 +1,47 @@
+#현재 계정정보를 가져옴
+data "aws_caller_identity" "current" {}
+
+
+provider "aws" {
+  region = "ap-northeast-2"
+  profile = "whs-sso-operation"
+}
+
+
+# GitHub Actions용 OIDC provider 설정
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com" # OIDC에서 사용할 클라이언트 ID
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1" # GitHub 공식 인증서 지문 thumbprint(공식값)
+  ]
+}
+
+# oidc_role 이라는 이름의 IAM Role
+resource "aws_iam_role" "oidc_role" {
+  name = var.role_name # 생성할 Role 이름
+
+  # GitHub에서 이 역할을 assume할 수 있게 설정
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn # 위에서 만든 OIDC Provider의 ARN
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            # 어떤 GitHub repo에서만 이 Role을 사용할 수 있는지 제어
+            "token.actions.githubusercontent.com:sub" : var.sub_condition
+          }
+        }
+      }
+    ]
+  })
+}
