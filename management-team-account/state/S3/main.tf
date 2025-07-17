ac6b17eb9320f88964079e3e5fd65c75a995c24a
subin <su0201bin@naver.com>
2025-07-18 06:31:57 +0900

    feat: S3 버킷에서 KMS 모듈 분리

--- management-team-account/state/S3/main.tf
+++ management-team-account/state/S3/main.tf
@@ -10,6 +10,13 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+# KMS 모듈 호출
+module "s3_kms" {
+  source      = "../../../modules/S3_kms"
+  description = "KMS key for S3 encryption"
+  s3_bucket_arn  = "arn:aws:s3:::cloudfence-management-state"
+}
+
 # S3 버킷 생성
 resource "aws_s3_bucket" "state_org" {
   bucket = "cloudfence-management-state"
@@ -51,57 +58,6 @@ resource "aws_s3_bucket_public_access_block" "state_org_block" {
   restrict_public_buckets = true
 }
 
-
-data "aws_caller_identity" "current" {}
-
-
-# S3 암호화를 위한 KMS 키
-resource "aws_kms_key" "s3_key" {
-  description         = "KMS key for S3 encryption"
-  enable_key_rotation = true
-
-  # KMS 키 정책 추가
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-
-      # 현재 계정에게 모든 KMS 작업 권한 부여
-      {
-        Sid    = "AllowRootAccountFullAccess",
-        Effect = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        },
-        Action   = "kms:*",
-        Resource = "*"
-      },
-
-      # S3 서비스에게 암복호화 권한 부여
-      {
-        Sid    = "AllowS3ServicePrincipal"
-        Effect = "Allow"
-        Principal = {
-          Service = "s3.amazonaws.com"
-        }
-        Action = [
-          "kms:Encrypt",
-          "kms:Decrypt",
-          "kms:ReEncrypt*",
-          "kms:GenerateDataKey*",
-          "kms:DescribeKey"
-        ]
-        Resource = "*"
-        Condition = {
-          StringEquals = {
-            "aws:SourceArn"     = "arn:aws:s3:::cloudfence-management-state",
-            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
-          }
-        }
-      }
-    ]
-  })
-}
-
 # S3 버킷 서버 측 암호화
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
   bucket = aws_s3_bucket.state_org.id
@@ -109,7 +65,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
-      kms_master_key_id = aws_kms_key.s3_key.arn
+      kms_master_key_id = module.s3_kms.kms_key_arn
     }
   }
 }
